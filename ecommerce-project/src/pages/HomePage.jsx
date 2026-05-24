8b2640469a4c978c9d0edde5b5f1eb71e45dbaad
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-05-24 20:12:48 +0700

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
+import { products } from '../../starting-code/data/products';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products'
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+      });
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
