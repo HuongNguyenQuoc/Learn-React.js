5bfd2880dda0892fd8c41ee807ddba3fa476e325
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-05-24 17:49:28 +0700

    Move orders page to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,15 +1,19 @@
-import { HomePage } from './pages/HomePage';
+import { Route, Routes } from 'react-router';
 import './App.css';
-import { Routes, Route } from 'react-router';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { HomePage } from './pages/HomePage';
+import { OrdersPage } from './pages/OrdersPage';
+import { Tracking } from './pages/Tracking';
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<Tracking />} />
     </Routes>
-    
+
   )
 }
 
