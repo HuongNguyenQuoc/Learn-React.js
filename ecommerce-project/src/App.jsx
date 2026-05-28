1148ea44739b33f45bd28beb650e78cf6e1ac9ca
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-05-28 16:37:02 +0700

    Install and replace some source in the code

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router';
+import { useEffect, useState, useCallback } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { HomePage } from './pages/home/HomePage';
@@ -10,22 +10,28 @@ import { Tracking } from './pages/Tracking';
 function App() {
   const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    const getCartItems = async () => {
+  const loadCart = useCallback(async () => {
+    try {
       const response = await axios.get('/api/cart-items?expand=product');
       setCart(response.data);
+    } catch (err) {
+      console.error(err);
     }
-    getCartItems();
+  }, []);
+
+  useEffect(() => {
+    axios.get('/api/cart-items?expand=product')
+      .then(res => setCart(res.data))
+      .catch(err => console.error(err));
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<Tracking />} />
     </Routes>
-
   )
 }
 
